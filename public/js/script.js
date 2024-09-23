a491a249fe2e00cc5bf89df00b942584ee5d3f52
Hiago Braga <hiagoaugustobraga@gmail.com>
2024-09-22 22:59:16 -0300

    refactor: redoing cleaner code

---
+++ public/js/script.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const apiBaseUrl = 'https://api-document-prime-url';
+
+    const recommendationsSection = document.getElementById('recommendations-list');
+    const topRatedSection = document.getElementById('top-rated-list');
+    const recentReleasesSection = document.getElementById('recent-releases-list');
+
+    
+    function handleError(error) {
+        console.error('Erro ao carregar dados da API:', error);
+    }
+
+    
+    function renderDocumentaryList(section, documentaries) {
+        documentaries.forEach(doc => {
+            const docCard = document.createElement('div');
+            docCard.classList.add('card');
+
+            docCard.innerHTML = `
+                <h3>${doc.titulo}</h3>
+                <p>${doc.descricao}</p>
+            `;
+
+            section.appendChild(docCard);
+        });
+    }
+
+    
+    axios.get(`${apiBaseUrl}/sugestoes`)
+        .then(response => {
+            const documentaries = response.data;
+            renderDocumentaryList(recommendationsSection, documentaries);
+        })
+        .catch(handleError);
+
+
+    axios.get(`${apiBaseUrl}/top-rated`)
+        .then(response => {
+            const topRatedDocumentaries = response.data;
+            renderDocumentaryList(topRatedSection, topRatedDocumentaries);
+        })
+        .catch(handleError);
+
+
+    axios.get(`${apiBaseUrl}/recent-releases`)
+        .then(response => {
+            const recentDocumentaries = response.data;
+            renderDocumentaryList(recentReleasesSection, recentDocumentaries);
+        })
+        .catch(handleError);
+});
